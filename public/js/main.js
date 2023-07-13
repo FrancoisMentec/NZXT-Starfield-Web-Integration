16415f29a9f962e02ad39096fa08da2c0e18a994
François Mentec <francois.mentec@alten.com>
2023-07-13 13:56:32 +0200

    work

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,42 @@
-import { MONTHS, get_cookie } from './util.js'
+import { DAYS, MONTHS, get_cookie } from './util.js'
 
 const VIEWSTATE = parseInt(get_cookie('viewstate')) || 640
 
 const svg = document.getElementById('svg')
 
 // Set dimensions
-document.documentElement.style.width = VIEWSTATE + 'px'
-document.documentElement.style.height = VIEWSTATE + 'px'
+/*document.documentElement.style.width = VIEWSTATE + 'px'
+document.documentElement.style.height = VIEWSTATE + 'px'*/
 document.body.style.width = VIEWSTATE + 'px'
 document.body.style.height = VIEWSTATE + 'px'
 svg.setAttribute('width', VIEWSTATE)
 svg.setAttribute('height', VIEWSTATE)
 
-
+// Temps update
 window.nzxt = {
     v1: {
         onMonitoringDataUpdate: (data) => {
             const { cpus, gpus, ram } = data // https://github.com/NZXTCorp/web-integrations-types/blob/main/v1/index.d.ts
-            /*update_cpu(cpus[0].temperature)
-            update_gpu(gpus[0].temperature)*/
+            update_cpu(cpus[0].temperature)
+            update_gpu(gpus[0].temperature)
         }
     }
 }
 
+const cpu_temp = document.getElementById('cpu_temp')
+
+function update_cpu (temp) {
+    cpu_temp.innerText = `${Math.round(temp)}°C`
+
+}
+
+const gpu_temp = document.getElementById('gpu_temp')
+
+function update_gpu (temp) {
+    gpu_temp.innerText = `${Math.round(temp)}°C`
+}
+
+// Rainbow
 function draw_rainbow (width, start_x, start_y, end_x, end_y) {
     const hw = width / 2
     const o = end_y - start_y
@@ -50,6 +64,12 @@ function draw_rainbow (width, start_x, start_y, end_x, end_y) {
         const offset = width / 8 + width / 4 * i
         set_ray(`ray_${i + 1}`, x1 + offset * Math.cos(angle_2), y1 - offset * Math.sin(angle_2), x2 + offset * Math.cos(angle_2), y2 - offset * Math.sin(angle_2), width / 4)
     }
+
+    // Date (follow the rainbow)
+    const dist = hw + 2 // We add the distance to the rainbow bottom to the rainbow half width
+    document.getElementById('date_path').setAttribute('d', `
+    M ${end_x + dist * Math.cos(angle_2)}, ${end_y - dist * Math.sin(angle_2)}
+    L ${start_x + dist * Math.cos(angle_2)}, ${start_y - dist * Math.sin(angle_2)}`)
 }
 
 function set_ray (name, start_x, start_y, end_x, end_y, width) {
@@ -62,3 +82,17 @@ function set_ray (name, start_x, start_y, end_x, end_y, width) {
 }
 
 draw_rainbow(30, 100, 0, 50, 50)
+
+
+// Date
+const YEAR_OFFSET = 300
+
+function update_date () {
+    const now = new Date()
+    document.getElementById('date').innerHTML = `${DAYS[now.getDay()]} ${now.getDate()} ${MONTHS[now.getMonth()]} ${now.getFullYear() + YEAR_OFFSET} CE`
+    const tomorrow = new Date()
+    tomorrow.setHours(24, 0, 0, 0)
+    setTimeout(update_date, tomorrow - now)
+}
+
+update_date()
